7205380a44c51dea256c513743fca160f33d132f
Luciano Griffith <luciano.griffith@bazimo.fr>
2024-12-30 22:19:40 +0100

    feat(testing): E2E testing

--- internal/assert/assert.go
+++ internal/assert/assert.go
@@ -1,11 +1,22 @@
 package assert
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func Equal[T comparable](t *testing.T, actual, expected T) {
 	t.Helper()
 
 	if actual != expected {
+		t.Errorf("expected %v, got %v", expected, actual)
+	}
+}
+
+func StringContains(t *testing.T, actual, expected string) {
+	t.Helper()
+
+	if !strings.Contains(actual, expected) {
 		t.Errorf("expected %v, got %v", expected, actual)
 	}
 }
